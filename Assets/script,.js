6dc1e055ab540e3b57c30cfea0cfd0adc7779de1
Saacmr <tristenwinton4@gmail.com>
2023-07-24 18:26:18 -0500

    html with some style

--- Assets/script,.js
+++ Assets/script,.js
@@ -1,1 +1,7 @@
+let currentCity = "#current-city"
+let currentTemp =  "current-temp"
+let currentHumidity = "current-humidity"
+let currentWindSpeed = "current-wind-speed"
 
+searchCityInput = "search-city"
+searchCityButton = "search-city-button"
